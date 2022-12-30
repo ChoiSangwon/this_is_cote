1aaa02fb95d56fbdfb4df37c29cca255ba4ea687
fdsa20040200 <fdsa200@naver.com>
2022-12-30 17:21:33 +0900

    add : 정렬된 배열에서 특정 수의 개수 구하기.py

---
+++ 유형벌 기출문제/27_정렬된 배열에서 특정 수의 개수 구하기.py
@@ -0,0 +1,11 @@
+from bisect import bisect_left, bisect_right
+
+N, x = map(int, input().split())
+arr = list(map(int, input().split()))
+
+left = bisect_left(arr, x)
+right =bisect_right(arr, x)
+if(right-left==0):
+    print(-1)
+else:
+    print(right-left)
