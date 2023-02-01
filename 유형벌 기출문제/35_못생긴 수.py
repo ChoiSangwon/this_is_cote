f67d24d4fc5a47b119155e9c090671de281b10d6
fdsa20040200 <fdsa200@naver.com>
2023-02-01 17:47:57 +0900

    35 못생긴 수.py

---
+++ 유형벌 기출문제/35_못생긴 수.py
@@ -0,0 +1,19 @@
+n = int(input())
+arr= [0]*n
+arr[0]= 1
+count=[0,0,0]
+next2, next3, next5 = 2, 3, 5
+
+for i in range(1, n):
+    arr[i] = min(next2, next3, next5)
+    if arr[i]==next2:
+        count[0]+=1
+        next2 = arr[count[0]]*2
+    if arr[i]==next3:
+        count[1]+=1
+        next3 = arr[count[1]]*3
+    if arr[i]==next5:
+        count[2]+=1
+        next5 = arr[count[2]]*5
+        
+print(arr)
